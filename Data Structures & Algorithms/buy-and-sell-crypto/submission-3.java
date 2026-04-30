d124c739f7d5fa7ff211f49f85090437c9acb8dc
Khairul Amiruf <khairul.amiruf@gmail.com>
2026-04-30 14:46:31 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0; 
+        int r = l +1;
+        int maxProfit = 0;
+
+        while (r < prices.length) {
+            int profit = prices[r] - prices[l];
+
+            if (profit < 0) {
+                l = r;
+            } else {
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            r += 1;
+        }
+
+        return maxProfit;
+    }
+}
